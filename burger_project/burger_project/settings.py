8f1d48673d282ce96f7b4c0c46744c5d4e8b9481
NurdaT <turdykul.nurdaulet@gmail.com>
2025-04-29 01:41:52 +0500

    Cooommit

--- burger_project/burger_project/settings.py
+++ burger_project/burger_project/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-wnc7%%ga7(c2hl_4z@))8=#^u7*3^e$mkia$e-e5alby%gd7z!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
